c16f8193a8ed490b196d84ab997988c43c00e762
Sac <sac.le@primedata.ai>
2022-05-13 10:35:46 +0700

    update code

--- bai1.cpp
+++ bai1.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<conio.h>
+#include<iostream>
 #include<malloc.h>
 #include<stdlib.h>
 #include<string.h>
@@ -12,21 +12,46 @@ typedef struct{
 	double thuong;
 	double phat;
 	double hsluong;
-}nhanvien;
+	double thuclinh;
+} nhanvien;
 
+using namespace std;
+
+double heSoLuongCaoNhat = 0;
+double thucLanhNuCaoNhat = 0;
 
 void nhap(nhanvien *a, int &n){
-	printf("\nNhap so luong nhan vien"); scanf("%d", &n);
+	printf("\nNhap so luong nhan vien: "); scanf("%d", &n);
 	for(int i=0; i<n; i++){
 		printf("\nnhan vien thu %d\n", i+1); fflush(stdin);
-		printf("ho ten:"); gets(a[i].hoten);
-		printf("Ma nv:"); gets(a[i].manv);
-		printf("gioi tinh:"); scanf("%d", &a[i].gt);
+		printf("ho ten:"); cin>>a[i].hoten;
+		printf("Ma nv:"); cin>>a[i].manv;
+		printf("goi tinh:"); scanf("%d", &a[i].gt);
 		printf("luong:"); scanf("%lf", &a[i].luong);
 		printf("thuong:"); scanf("%lf", &a[i].thuong);
 		printf("phat:"); scanf("%lf", &a[i].phat);
 		printf("he so luong:"); scanf("%lf", &a[i].hsluong);
+
+		a[i].thuclinh = a[i].luong + a[i].thuong - a[i].phat;
+		// tim tien thuc lanh cua nu cao nhat
+		if (a[i].gt == 1 && a[i].thuclinh > thucLanhNuCaoNhat) {
+			thucLanhNuCaoNhat = a[i].thuclinh;
+		}
+
+		if (a[i].hsluong > heSoLuongCaoNhat) {
+			heSoLuongCaoNhat = a[i].hsluong;
+		}
 	}
+}
+
+void hienThiThongTin(nhanvien nv) {
+	printf("\n ======= Thong tin nhan vien %10s ==== \n", nv.manv);
+	printf("Ho ten: %30s\n", nv.hoten);
+	printf("Gioi tinh: %3s\n", (nv.gt == 0 ? "nam" : "nu ") );
+	printf("Luong: %30lf\n", nv.luong);
+	printf("Thuong: %30lf\n", nv.thuong);
+	printf("Phat: %30lf\n", nv.phat);
+	printf("He so: %30lf\n", nv.hsluong);
 }
 
 void xuatfile(nhanvien *a, int n, FILE*f){
@@ -41,76 +66,34 @@ void xuatfile(nhanvien *a, int n, FILE*f){
 }
 
 void hsluongmax(nhanvien *a, int n){
-	double m= a[0].hsluong;
-	for(int i=0; i<n; i++)
-		if(a[i].hsluong>m) m=a[i].hsluong;
-	for(int i=0; i<n; i++)
-		if(a[i].hsluong==m){
-			printf("\n%4d%30s%10s",i+1,a[i].hoten,a[i].manv);
-			if(a[i].gt==0)  printf(" nam  ");
-				else printf(" nu  ");
-			printf("\n%30lf%30lf%30lf%30lf",a[i].luong,a[i].thuong,a[i].phat,a[i].hsluong);
+	for(int i=0; i<n; i++) {
+		if(a[i].hsluong == heSoLuongCaoNhat){
+			hienThiThongTin(a[i]);
 		}  
+	}
 }
 
 void thuclinhnumax(nhanvien *a, int n){
-	double m;
-	for(int i=0; i<n; i++)
-		if(a[i].gt==1){
-			m = a[i].thuong+a[i].luong-a[i].phat;
-			break;
+	for(int i=0; i<n; i++) {
+		if (a[i].gt == 0) {
+			continue;
 		}
-	for(int i=0; i<n; i++)
-		if(a[i].thuong+a[i].luong-a[i].phat>m && a[i].gt==1){
-			m=a[i].thuong+a[i].luong-a[i].phat;
-		}
-	for(int i=0; i<n; i++){
-		if(a[i].thuong+a[i].luong-a[i].phat==m && a[i].gt==1){
-			printf("\n%4d%30s%10s",i+1,a[i].hoten,a[i].manv);
-			if(a[i].gt==0)  printf(" nam  ");
-				else printf(" nu  ");
-			printf("\n%30lf%30lf%30lf%30lf",a[i].luong,a[i].thuong,a[i].phat,a[i].hsluong);
+
+		if(a[i].thuclinh > thucLanhNuCaoNhat){
+			hienThiThongTin(a[i]);
 		}
 	}
 }
 
-void luonglonhon(nhanvien *a, int n, double x){
-	for(int i=0; i<n; i++)
-		if(a[i].luong>=x){
-			printf("\n%4d%30s%10s",i+1,a[i].hoten,a[i].manv);
-				if(a[i].gt==0)  printf(" nam  ");
-					else printf(" nu  ");
-			printf("\n%30lf%30lf%30lf%30lf",a[i].luong,a[i].thuong,a[i].phat,a[i].hsluong);
-		}
-	
-}
-
-void ttnhanvien(nhanvien *a, int n, char z[30]){
-	for(int i=0; i<n; i++)
-		if (strcmp(a[i].hoten,z)==0){
-			printf("\n%4d%30s%10s",i+1,a[i].hoten,a[i].manv);
-				if(a[i].gt==0)  printf(" nam  ");
-					else printf(" nu  ");
-			printf("\n%30lf%30lf%30lf%30lf",a[i].luong,a[i].thuong,a[i].phat,a[i].hsluong);
-	}
-}
 int main(){
 	nhanvien *a;
 	int n;
-	double x; char z[30];
 	FILE*f;
 	a=(nhanvien*)malloc(20*sizeof(nhanvien));
 	nhap(a,n);
 	xuatfile(a,n,f);
-	printf("nhan vien he so luong cao nhat");
 	hsluongmax(a,n);
-	printf("nhan vien nu thuc linh cao nhat");
 	thuclinhnumax(a,n);
-	printf("luong nhan vien lon hon hoac bang"); scanf("%lf", &x);
-	luonglonhon(a,n,x);
-	fflush(stdin);
-	printf("nhap ten nhan vien");  gets(z);
-	ttnhanvien(a,n,z);
 	return 0;
 }
 
